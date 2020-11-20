c889e5d3bd238d52365b74f2123565ae5017ac0a
Jan Gleixner <jan.gleixner@gmail.com>
2020-11-20 20:51:08 +0100

    make code more similar to alorithm pseudo code

--- src/Best1990.h
+++ src/Best1990.h
@@ -10,20 +10,33 @@
 #include <vector>
 
 template <class T>
+typename T::value_type inline avg(T rCS, std::size_t i, std::size_t i2) {
+    return (rCS[i2]-rCS[i])/(i-i2);                                                                      // avg <- function(i, i2=n+1) (rCS[i]-rCS[i2])/(i2-i)
+}
+
+template <class T>
+typename T::value_type inline avgB(T rCS, std::forward_list<std::size_t>::iterator B) {                  // avgB <- function(B) avg(J[B], J[B+1])
+    std::size_t i = *B;
+    std::size_t i2 = *(B++);
+    return avg(rCS, i, i2);
+
+}
+
+template <class T>
 T isoreg_Best1990(T y) {
     const std::size_t n = y.size();                                                                      // n <- length(y)
     std::vector<typename T::value_type> rCS(n+1);                                                        //
     std::partial_sum(y.rbegin(), y.rend(), ++rCS.rbegin());                                              // rCS <- c(rev(cumsum(rev(y))), 0)
-    std::forward_list<std::size_t> J;                                                                    // J <- c(1L, n+1L)
-    std::forward_list<std::size_t>::iterator B_, B_1;                                                    // avg <- function(i, i2=n+1) (rCS[i]-rCS[i2])/(i2-i)
-    typename T::value_type avg_B0 = rCS[0]/n;                                                            // avgB <- function(B) avg(J[B], J[B+1])
-    J.push_front(0);                                                                                     // B0 = 1
+    std::forward_list<std::size_t> J;  // current active set J                                            //
+    std::forward_list<std::size_t>::iterator B_, B_1;                                                    //
+    typename T::value_type avg_B0 = rCS[0]/n;  // == avg(rCS, 0 ,n) == avb(0) == av(B0)                  // B0 = 1
+    J.push_front(0);                                                                                     // J <- c(1L, n+1L)
     for (std::size_t i = 1; i<n; i++) {                                                                  // for (i in seq_along(y)) {
-        if (rCS[i]/(n-i) > avg_B0) {                                                                     //     if (avg(i) > avgB(B0)) {
+        if ((rCS[i] /* - rCS[n] */)/(n-i) > avg_B0) { // avg(i, n) > avg                                 //     if (avg(i) > avgB(B0)) {
             B_ = J.begin();                                                                              //         J <- c(J[-(B0+1)], i, J[B0+1])
             B_1 = J.begin()++;                                                                           //         B_ <- B0
             avg_B0 = rCS[i]/(n-i);                                                                       //         B0 <- B0+1
-            while (B_1 != J.end() && (rCS[*B_]-rCS[i])/(i-*B_1) <= (rCS[*B_1]-rCS[*B_])/(*B_-*B_1)) {    //         while (B_ > 1 && avgB(B_) < avgB(B_-1)) {
+            while (B_1 != J.end() && avg(rCS, *B_, *B_1) < avg(rCS, i, *B_)) {                           //         while (B_ > 1 && avgB(B_) < avgB(B_-1)) {
                 B_++; B_1++;                                                                             //             J <- J[-B_]
                 J.pop_front();                                                                           //             B_ <- B_-1
             }                                                                                            //             B0 <- B0-1 # only because we use an array for J
@@ -35,7 +48,7 @@ T isoreg_Best1990(T y) {
     T yf(y.size());                                                                                      // yf <- rep(avgB(seq_len(length(J)-1)), J[-1]-J[-length(J)])
     for(std::size_t i, prev_i = n; J.begin() != J.end(); J.pop_front()){                                 //
         i = *J.begin();                                                                                  //
-        std::fill(yf.begin()+i, yf.begin()+prev_i, (rCS[i]-rCS[prev_i])/(prev_i-i));                     //
+        std::fill(yf.begin()+i, yf.begin()+prev_i, avg(rCS, prev_i, i));                     //
         prev_i = i;                                                                                      //
     }                                                                                                    //
     return yf;                                                                                           //
